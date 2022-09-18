7f7cc8a494ab9eb4a298026419a51be440129c57
camelcrush <dkxm0927@naver.com>
2022-09-18 14:01:47 +0900

    Blockchain

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,37 @@ class Block implements BlockShape {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
   static calculateHash(prevHash: string, height: number, data: string) {
-    return `${prevHash}${height}${data}`;
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockhain = new Blockchain();
+
+blockhain.addBlock("First one");
+blockhain.addBlock("Second One");
+blockhain.addBlock("Third one");
+
+console.log(blockhain.getBlocks());
